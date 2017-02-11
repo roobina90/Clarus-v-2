6523480e1a5ffc52b4acf30be72aceffa93d06d1
roobina90 <roobina1990@gmail.com>
2017-02-11 11:13:59 +0100

    gulp small fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
  
 gulp.task('less', function () {
-  return gulp.src('./style/**/*.less')
+  return gulp.src('./style/style.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
